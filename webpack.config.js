94682232b48fd8cee5a718c56d7b3424fe701820
Pauli Perälä <perala.pauli@gmail.com>
2016-05-15 13:41:38 +0300

    typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 /**
  * Created by pperala on 15/05/16.
  */
+module.exports = {
+    entry: './app.ts',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}
